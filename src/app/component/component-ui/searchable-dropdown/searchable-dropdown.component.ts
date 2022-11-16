2d0411f80cace9caf89b50d30f985ec2fa0953f2
beska0013 <gugushvili.beso0013@gmail.com>
2022-11-17 00:23:01 +0400

    fix: section css alighnment

--- src/app/component/component-ui/searchable-dropdown/searchable-dropdown.component.ts
+++ src/app/component/component-ui/searchable-dropdown/searchable-dropdown.component.ts
@@ -9,16 +9,17 @@ import {
   ViewChild
 } from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormControl, ReactiveFormsModule} from "@angular/forms";
-import {NbAutocompleteModule, NbComponentSize, NbInputModule} from "@nebular/theme";
-import {EMPTY, map, Observable, of} from "rxjs";
+import { ReactiveFormsModule} from "@angular/forms";
+import { NbComponentSize} from "@nebular/theme";
+import { map, Observable, of, tap} from "rxjs";
 import {environment} from "../../../../environments/environment";
+import {NebularModule} from "../../../shared/nebular/nebular.module";
 
 
 @Component({
   selector: 'app-searchable-dropdown',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NbInputModule, NbAutocompleteModule],
+  imports: [CommonModule, ReactiveFormsModule,NebularModule],
   templateUrl: './searchable-dropdown.component.html',
   styleUrls: ['./searchable-dropdown.component.scss'],
   changeDetection:ChangeDetectionStrategy.OnPush
@@ -29,6 +30,7 @@ export class SearchableDropdownComponent implements OnInit, OnChanges {
 
   @ViewChild('autoInput') input;
 
+  @Input() allowAdd:boolean = false;
   @Input() dataType:string
   @Input() labelFor:string;
   @Input() title:string;
@@ -37,12 +39,14 @@ export class SearchableDropdownComponent implements OnInit, OnChanges {
   @Input() dropdownList:any[];
   @Input() form:any[];
   @Input() lastOption:{ID:number,Id:number, Title:string};
-  @Output() output = new EventEmitter<{type: string, value: string | number}>()
+  @Output() output = new EventEmitter<{type: string, value: string | number}>();
 
   //when data loads first time
   dataFirstChange = true;
   closeState = true;
   filteredOptions$: Observable<string[] | never>;
+  addItemState:boolean = false;
+  tooltip:string;
   private areaDataType = 'CHOrganizationBoundShadow:CH1BoundShadow:CH2BoundShadow:CH3BoundShadow:CH4BoundShadow:CH5BoundShadow:CH6BoundShadow:CH7BoundShadow:CH8BoundShadow:CH9BoundShadow:CH10BoundShadow'
 
   private onFieldstateChange(value:any, changeType:string){
@@ -81,7 +85,13 @@ export class SearchableDropdownComponent implements OnInit, OnChanges {
     null
   }
   onInput(){
-    this.filteredOptions$ = this.getFilteredOptions(this.input.nativeElement.value);
+    this.filteredOptions$ = this.getFilteredOptions(this.input.nativeElement.value)
+      .pipe(
+        tap(list => {
+          this.addItemState = this.allowAdd && list.length <= 0;
+
+        })
+      );
   }
   onSelectionChange(event) {
     if(!(!!event)) return null
@@ -92,7 +102,7 @@ export class SearchableDropdownComponent implements OnInit, OnChanges {
   }
   onInputClick(){
     this.dataType !== this.areaDataType ? this.filteredOptions$ = of(this.dropdownList): null;
-    console.log('this.dropdownList',this.dropdownList);
+
   }
   onFirstInput(){
       this.closeState = false;
@@ -105,12 +115,16 @@ export class SearchableDropdownComponent implements OnInit, OnChanges {
   }
   trackBy =(index, item) => index
 
+  onAddNewItem(){
+    console.log(this.input.nativeElement.value);
+  }
 
 
   ngOnInit(): void {
 
 
     this.filteredOptions$ = of(this.dropdownList);
+    this.tooltip = `Add New ${this.title}`;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
